8bd11c31300c72aef44ba0723f9ccf7bc932db46
Safetytrainer <ergonom@list.ru>
2020-09-03 00:07:16 +0300

    07_OOP valid+format

--- JS_oop/JS_01/main.js
+++ JS_oop/JS_01/main.js
@@ -1,95 +1,71 @@
-// "use strict";
+"use strict";
 
-// базовый конструктор «Транспортное средство» с общими для всех транспортных средств свойствами 
-// и методами (на ваше усмотрение);
-// дочерние конструкторы: автомобиль, самолет, корабль, которые наследуют общие свойства 
-// и методы от родительского;
-// продемонстрировать, как дочерние сущности могут переопределять родительские свойства 
-// и методы в соответствии со своим собственным поведением
 
-// var totalUnits = prompt('Всего едениц техники');
-// var repairsUnits = prompt('Всего едениц техники в ремонте');
-// var operatedUnits = prompt('Всего едениц техники в работе');
-
-// var vehicles = new Vehicles(13, 3, 9);
-// console.log(vehicles);
-// console.log(vehicles.getNotUsesUnits());
-// var aircraftUnits = new Aircraft(3, 1, 2, 30);
-// console.log(aircraftUnits);
-// console.log(aircraftUnits.getNotUsesUnits());
-// var carUnits = new Car(7, 2, 4, 20);
-// console.log(carUnits);
-// console.log(carUnits.getNotUsesUnits());
-// var shipUnits = new Ships(2, 0, 2, 10);
-// console.log(shipUnits);
-// console.log(shipUnits.getNotUsesUnits());
-
-function Vehicles(totalUnits, repairsUnits, operatedUnits)
-{
-  this.totalUnits = totalUnits;
-  this.repairsUnits = repairsUnits;
-  this.operatedUnits = operatedUnits;
-  this.getNotUsesUnits = function() {
-    return (this.totalUnits - this.repairsUnits - this.operatedUnits);
-  }
+function Vehicles(totalUnits, repairsUnits, operatedUnits) {
+	this.totalUnits = totalUnits;
+	this.repairsUnits = repairsUnits;
+	this.operatedUnits = operatedUnits;
+	this.getNotUsesUnits = function () {
+		return (this.totalUnits - this.repairsUnits - this.operatedUnits);
+	}
 }
 
 
 function Aircraft(totalUnits, repairsUnits, operatedUnits, parachute) {
-    Vehicles.call(this, totalUnits, repairsUnits, operatedUnits);
+	Vehicles.call(this, totalUnits, repairsUnits, operatedUnits);
 
-    this.parachute = parachute;
-    alert(parachute + ' Парашютов на складе');
-    
-    this.typeVehicl = 'Самолеты';
+	this.parachute = parachute;
+	alert(parachute + ' Парашютов на складе');
 
-    this.changTotalUnits = function (total) {
-        this.totalUnits = total;
-    }
-    this.changRepairsUnits = function (repairs) {
-        this.repairsUnits = repairs;
-    }
-    this.changOperatedUnits = function (operated) {
-        this.operatedUnits = operated;
-    }
+	this.typeVehicl = 'Самолеты';
+
+	this.changTotalUnits = function (total) {
+		this.totalUnits = total;
+	}
+	this.changRepairsUnits = function (repairs) {
+		this.repairsUnits = repairs;
+	}
+	this.changOperatedUnits = function (operated) {
+		this.operatedUnits = operated;
+	}
 }
 
 function Car(totalUnits, repairsUnits, operatedUnits, wheels) {
-    Vehicles.call(this, totalUnits, repairsUnits, operatedUnits);
-    
-    this.wheels = wheels;
-    alert(wheels + ' Колес на складе');
-    this.typeVehicl = 'Авто';
+	Vehicles.call(this, totalUnits, repairsUnits, operatedUnits);
 
-    this.changTotalUnits = function (total) {
-        this.totalUnits = total;
-    }
-    this.changRepairsUnits = function (repairs) {
-        this.repairsUnits = repairs;
-    }
-    this.changOperatedUnits = function (operated) {
-        this.operatedUnits = operated;
-    }
+	this.wheels = wheels;
+	alert(wheels + ' Колес на складе');
+	this.typeVehicl = 'Авто';
+
+	this.changTotalUnits = function (total) {
+		this.totalUnits = total;
+	}
+	this.changRepairsUnits = function (repairs) {
+		this.repairsUnits = repairs;
+	}
+	this.changOperatedUnits = function (operated) {
+		this.operatedUnits = operated;
+	}
 }
 
 function Ships(totalUnits, repairsUnits, operatedUnits, boats) {
-    Vehicles.apply(this, arguments);
+	Vehicles.apply(this, arguments);
 
-    this.boats = boats;
-    alert(boats + ' Лодок на складе');
-    
-    this.typeVehicl = 'Корабли';
-    
+	this.boats = boats;
+	alert(boats + ' Лодок на складе');
 
-    this.changTotalUnits = function (total) {
-        this.totalUnits = total;
-    }
-    this.changRepairsUnits = function (repairs) {
-        this.repairsUnits = repairs;
-    }
-    this.changOperatedUnits = function (operated) {
-        this.operatedUnits = operated;
-    }
+	this.typeVehicl = 'Корабли';
+
+
+	this.changTotalUnits = function (total) {
+		this.totalUnits = total;
+	}
+	this.changRepairsUnits = function (repairs) {
+		this.repairsUnits = repairs;
+	}
+	this.changOperatedUnits = function (operated) {
+		this.operatedUnits = operated;
+	}
 }
 
 var vehicles = new Vehicles(13, 3, 9);
@@ -100,23 +76,23 @@ var aircraftUnits = new Aircraft(3, 1, 2, 30);
 console.log(aircraftUnits);
 console.log(aircraftUnits.getNotUsesUnits() + ' Простой техники');
 if (prompt('ОК - изменить параметры') === '') {
-aircraftUnits.changTotalUnits(prompt('Общее колличетво поменять самолеты'));
-aircraftUnits.changRepairsUnits(prompt('Ремонтный цех поменять самолеты'));
-aircraftUnits.changOperatedUnits(prompt('В эксплуатации поменять самолеты'));
+	aircraftUnits.changTotalUnits(prompt('Общее колличетво поменять самолеты'));
+	aircraftUnits.changRepairsUnits(prompt('Ремонтный цех поменять самолеты'));
+	aircraftUnits.changOperatedUnits(prompt('В эксплуатации поменять самолеты'));
 }
 var carUnits = new Car(7, 2, 4, 20);
 console.log(carUnits);
 console.log(carUnits.getNotUsesUnits() + ' Простой техники');
 if (prompt('ОК - изменить параметры') === '') {
-carUnits.changTotalUnits(prompt('Общее колличетво поменять машины'));
-carUnits.changRepairsUnits(prompt('Ремонтный цех поменять машины'));
-carUnits.changOperatedUnits(prompt('В эксплуатации поменять машины'));
+	carUnits.changTotalUnits(prompt('Общее колличетво поменять машины'));
+	carUnits.changRepairsUnits(prompt('Ремонтный цех поменять машины'));
+	carUnits.changOperatedUnits(prompt('В эксплуатации поменять машины'));
 }
 var shipUnits = new Ships(2, 0, 2, 10);
 console.log(shipUnits);
 console.log(shipUnits.getNotUsesUnits() + ' Простой техники');
 if (prompt('ОК - изменить параметры') === '') {
-shipUnits.changTotalUnits(prompt('Общее колличетво поменять коробли'));
-shipUnits.changRepairsUnits(prompt('Ремонтный цех поменять коробли'));
-shipUnits.changOperatedUnits(prompt('В эксплуатации поменять коробли'));
+	shipUnits.changTotalUnits(prompt('Общее колличетво поменять коробли'));
+	shipUnits.changRepairsUnits(prompt('Ремонтный цех поменять коробли'));
+	shipUnits.changOperatedUnits(prompt('В эксплуатации поменять коробли'));
 }
